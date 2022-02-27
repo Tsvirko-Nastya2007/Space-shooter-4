342e0e1e681b948acbcc75a4d3d0329a2ea56644
Tsvirko Nastya <92594745+Tsvirko-Nastya2007@users.noreply.github.com>
2022-02-27 19:29:55 +0300

    Space-shooter-4
    Space-shooter-4

---
+++ game.cpp
@@ -0,0 +1,94 @@
+#include "game.h"
+#include "settings.h"
+
+Game::Game():
+	window(sf::VideoMode(WINDOW_WIDTH,WINDOW_HEIGHT),
+		WINDOW_TITLE,
+		sf::Style::Titlebar | sf::Style::Close),
+	player_hp("PNG/kenvector_future_thin.ttf", 24, 550, 10, sf::Color::White),
+	game_over_screen(IMAGES_FOLDER+ GAME_OVER_FILE_NAME,1.351f,2.135f)
+{
+	window.setVerticalSyncEnabled(true);
+	for (size_t i = 0; i < METEORS_QTY; i++) {
+		Meteor* meteor = new Meteor();
+		meteors.push_back(meteor);
+	}
+}
+void Game::play() {
+	while (window.isOpen()) {
+		check_events();
+		update();
+		check_colisions();
+		draw();
+	}
+}
+void Game::check_events() {
+	sf::Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == sf::Event::Closed) window.close();
+		else
+			if (event.type == sf::Event::KeyPressed) 
+				if (game_state == PLAY && event.key.code == sf::Keyboard::Space) {
+					Laser* new_laser = new Laser(
+						player.getPosition().x+player.getHitBox().width/2,
+						player.getPosition().y + player.getHitBox().height / 2
+					);
+					lasers.push_back(new_laser);
+				}
+		
+	}
+}
+void Game::update() {
+	switch (game_state) {
+	case INTRO:
+		break;
+	case PLAY:
+		player.update();
+		for (size_t i = 0; i < METEORS_QTY; i++) {
+			meteors[i]->update();
+		}
+		for (auto iter = lasers.begin(); iter != lasers.end(); iter++) {
+			(*iter)->update();
+		}
+		lasers.remove_if([](Laser* laser) {return laser->getPosition().y < -50; });
+		player_hp.update(std::to_string(player.getHp()));
+		if (player.getHp() <= 0) {
+			game_state = GAME_OVER;
+		}
+		break;
+	case GAME_OVER:
+		break;
+	}
+}
+void Game::draw() {
+	window.clear(sf::Color::Black);
+	switch (game_state) {
+	case INTRO:
+		break;
+	case PLAY:
+		player.draw(window);
+		for (size_t i = 0; i < METEORS_QTY; i++) {
+			meteors[i]->draw(window);
+		}
+		for (auto iter = lasers.begin(); iter != lasers.end(); iter++) {
+			(*iter)->draw(window);
+		}
+		window.draw(player_hp.getText());
+		break;
+	case GAME_OVER:
+		window.draw(game_over_screen.getSprite());
+		break;
+	}
+
+	window.display();
+}
+
+void Game::check_colisions() {
+	//meteor popal v korabl
+	for (auto iter = meteors.begin(); iter != meteors.end(); iter++) {
+		if (player.getHitBox().intersects((*iter)->getHitBox())) {
+			(*iter)->spawn();
+			player.decreaseHp(static_cast <size_t>((*iter)->getHitBox().width/3));
+		}
+	}
+}
---
+++ laser.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+class Laser {
+private:
+	sf::Texture texture;
+	sf::Sprite sprite;
+public:
+	Laser(float x, float y);
+	void update();
+	void draw(sf::RenderWindow& window);
+	sf::FloatRect getHitBox();
+	sf::Vector2f getPosition();
+};
